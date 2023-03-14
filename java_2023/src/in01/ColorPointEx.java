f9684bb1a33233e1e576210ed39940c2fb444ed1
Hyunji Kim <papertoy01@gmail.com>
2023-03-14 09:16:36 +0900

    접근제어자

--- java_2023/src/in01/ColorPointEx.java
+++ java_2023/src/in01/ColorPointEx.java
@@ -4,7 +4,8 @@ public class ColorPointEx {
 	public static void main(String args[]) {
 		
 		Point_ex p = new Point_ex ();		//Point 객체 생성
-		p.set(1,2);					//
+		// p.x = 5;  --> 접근제어자 private!!
+		p.set(1,2);	
 		p.showPoint();
 		
 		ColorPoint cp = new ColorPoint();
