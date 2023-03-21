c78a9f07d9029fb42c45c9c08d766295579636c4
Mirim <Mirim@DESKTOP-MED021C>
2023-03-21 09:32:30 +0900

    inheritance point

---
+++ JavaPrg/src/in03/ColorPointEx.java
@@ -0,0 +1,17 @@
+package in03;
+
+public class ColorPointEx {
+	public static void main(String args[]) {
+		
+		Point_ex p = new Point_ex ();		//Point 객체 생성
+		// p.x = 5;  --> 접근제어자 private!!
+		p.set(1,2);	
+		p.showPoint();
+		
+		ColorPoint cp = new ColorPoint();
+		cp.set(3, 4);
+		cp.setColor("red");
+		cp.showColorPoint();
+	}
+
+}
