ac24d2fdef9dbc7537279b0920096451823fc29f
Mirim <Mirim@DESKTOP-MED021C>
2023-03-21 09:33:56 +0900

    Upcasting inheritance

---
+++ JavaPrg/src/in04/UpcastingEx.java
@@ -0,0 +1,20 @@
+package in04;
+
+public class UpcastingEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student s = new Student("이재문");
+		Person p;
+		p = s;		//업캐스팅
+		//p는 person클래스를 가리키는 객체변수이고
+		//p는 s객체를 할당받음
+		//
+		
+		System.out.println(p.name);
+		
+		p.grade = "A";
+		p.department = "Com";
+	}
+
+}
