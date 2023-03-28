fa4a95d8cfc241369ba6807fd81814d65dbdf7e5
Hyunji Kim <papertoy01@gmail.com>
2023-03-28 09:58:51 +0900

    add explain

--- 2023class/src/in10/PhoneInterface.java
+++ 2023class/src/in10/PhoneInterface.java
@@ -1,10 +1,10 @@
 package in10;
 
 public interface PhoneInterface {
-	final int TIMEOUT = 10000;
-	void sendCall();
-	void receiveCall();
-	default void printLogo() {
+	final int TIMEOUT = 10000;	//상수 (public static final이 생략)
+	void sendCall();			//추상메서드 (abstract 키워드 생략)
+	void receiveCall();			//추상메서드 (abstract 키워드 생략)
+	default void printLogo() {	// 개발자들의 요청에 의해서 oracle에서 기능 추가
 		System.out.println("**Phone**");
 	}
 }
