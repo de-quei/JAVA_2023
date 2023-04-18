8fadf5f64126d51cace5267ad128273028244c72
Hyunji Kim <papertoy01@gmail.com>
2023-04-18 09:55:59 +0900

    HashMap

---
+++ Pro03Collection/src/coll02/HashMapEx.java
@@ -0,0 +1,27 @@
+package coll02;
+
+import java.util.HashMap;
+import java.util.Scanner;
+public class HashMapEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HashMap<String, String> dic = new HashMap<String, String>();
+		
+		dic.put("baby", "아기");
+		dic.put("love", "사랑");
+		dic.put("apple", "사과");
+		
+		Scanner scanner = new Scanner(System.in);
+		while(true) {
+			System.out.print("찾고 싶은 단어 : ");
+			String eng = scanner.next();
+			if(eng.equals("exit")) {	//문자열 비교는 equals 메소드
+				System.out.println("종료");
+				break;
+			}
+		}
+		
+	}
+
+}
