24ffb87e589e511b901b75f6c9d6e40fe9e48b08
Hyunji Kim <papertoy01@gmail.com>
2023-05-09 09:15:34 +0900

    string

--- Pro02project/src/pack02/toStringEx.java
+++ Pro02project/src/pack02/toStringEx.java
@@ -3,7 +3,8 @@ package pack02;
 public class toStringEx {
 
 	public static void main(String[] args) {
-		Point p=new Point(2,3);
+		
+		Point p = new Point(2,3);
 		System.out.println(p.toString());
 		System.out.println(p); //.toString() 생략
 		System.out.println(p+"입니다");
