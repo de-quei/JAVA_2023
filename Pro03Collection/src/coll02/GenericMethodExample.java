6ed67fb3a3a87bb3e5433b9617b06c6f8429502c
Hyunji Kim <papertoy01@gmail.com>
2023-05-11 14:07:11 +0900

    파일 입출력

--- Pro03Collection/src/coll02/GenericMethodExample.java
+++ Pro03Collection/src/coll02/GenericMethodExample.java
@@ -26,6 +26,7 @@ public class GenericMethodExample {
 			//Double.valueOf(i)는 정수 i값을 실수형으로 변환.
 		}
 		gs = reverse(gs);
+		//gs는 reverse메소드의 리턴값을 받음
 		for(int i = 0; i < 5; i++) {
 			System.out.println(gs.pop());
 		}
