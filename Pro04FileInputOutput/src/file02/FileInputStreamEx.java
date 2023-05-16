647e5986ee7f58ee686a5b185b3efd2ee01cc67c
Hyunji Kim <papertoy01@gmail.com>
2023-05-16 10:13:57 +0900

    파일 인풋 스트림

---
+++ Pro04FileInputOutput/src/file02/FileInputStreamEx.java
@@ -0,0 +1,30 @@
+package file02;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class FileInputStreamEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte[] b = new byte[6];
+		
+		try {
+			FileInputStream fin  = new FileInputStream("C:\\Temp\\test.out");
+			//fin 입력파일 객체변수로 test.out파일을 가리킨다.
+			int n = 0, c;
+			while((c = fin.read()) != -1) { //fin이 가리키는 "test.out"파일에 있는 내용을 읽어서 c에 저장한다.
+				b[n] = (byte)c; //byte타입으로 형변환하여 배열 b에 저장한다.
+				n++; //배열의 인덱스 증가하기 위함
+			}
+			System.out.println("C:\\Temp\\test.out에서 읽은 배열을 출력합니다.");
+			for(int i = 0; i < b.length; i++)
+				System.out.print(b[i] + " ");
+			System.out.println();
+			fin.close();
+		}catch(IOException e) {
+			System.out.println("C:\\\\Temp\\\\test.out에서 읽지 못했습니다.");
+		}
+	}
+
+}
