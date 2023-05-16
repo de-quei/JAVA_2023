90100bf0134e214d56d2d6fe862cf5a02736cc0b
Hyunji Kim <papertoy01@gmail.com>
2023-05-16 09:03:54 +0900

    최종

--- Pro04FileInputOutput/src/file01/FileReaderEx.java
+++ Pro04FileInputOutput/src/file01/FileReaderEx.java
@@ -10,10 +10,12 @@ public class FileReaderEx {
 		//FileReader fin = new FileReader("C:\\Windows.system.ini");
 		
 		try { // 파일 입출력에 관계되는 모든 프로그램은 try ~ catch문을 사용해야 한다.
-			//try~catch 안에 있는 것을 실행하다가 오류가 발생하면 catch를 실행하고, 만약에 오류가 없다면 catch문장은 실행하지 않는다.
+			//try~catch 안에 있는 것을 실행하다가 오류가 발생하면 catch를 실행하고
+			//만약에 오류가 없다면 catch문장은 실행하지 않는다.
 			fin = new FileReader("C:\\Windows\\system.ini");
 			int c;
-			//fin이 가리키는 파일(system.ini)에서 한 문자씩 읽어서 c에 저장하고, fin객체변수가 파일의 끝에 도달하면 -1의 값을 c에게 넘겨준다.
+			//fin이 가리키는 파일(system.ini)에서 한 문자씩 읽어서 c에 저장하고
+			//fin객체변수가 파일의 끝에 도달하면 -1의 값을 c에게 넘겨준다.
 			//반환하면 파일의 끝까지 읽어서 화면에 출력한다.
 			while((c = fin.read()) != -1) {
 				System.out.print((char)c);
